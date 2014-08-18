0d16b01befe65d2ddd237cd35b58a5ce290fa3f3
jamamel <jamamel@gmail.com>
2014-08-18 02:31:05 +0100

    v0.1 Final commit
    Final commit of v0.1, including Code Book and run_analysisf.R function
    wrapper.

---
+++ CodeBook.md
@@ -0,0 +1,96 @@
+#Code Book
+
+This document describes the data set produced by the script [run.analysis.R](https://github.com/Jamamel/getcleanData/blob/master/run_analysis.R). It reads in a collection of datasets, split by subject group **test** or **train**. There are **2 consistently formatted groups of 12 datasets** per group.
+
+For specifics about the contents of each data set please refer to the original study's [Code Book](https://github.com/Jamamel/getcleanData/blob/master/UCI%20HAR%20Dataset/README.txt).
+
+Each data set was named by extracting the generic roots from their file name. (e.g. `X_test.txt` became `X`, row 11 in table below)
+
+```R
+List of 2
+ $ test :List of 12
+ $ train:List of 12
+```
+
+```R
+Total: 42MB
+           NAME  NROW MB
+ 1:  body_acc_x 2,947  3
+ 2:  body_acc_y 2,947  3
+ 3:  body_acc_z 2,947  3
+ 4: body_gyro_x 2,947  3
+ 5: body_gyro_y 2,947  3
+ 6: body_gyro_z 2,947  3
+ 7:     subject 2,947  1
+ 8: total_acc_x 2,947  3
+ 9: total_acc_y 2,947  3
+10: total_acc_z 2,947  3
+11:           X 2,947 13
+12:           y 2,947  1
+```
+
+The output of this script is a data table (in `*.txt` format), of which this is a preview:
+
+```R
+      Group SubjectID Activity              feature        mean        std
+   1:  test         2  walking             fBodyAcc -0.27391978 -0.3606977
+   2:  test         2  walking         fBodyAccJerk -0.31277871 -0.3151814
+   3:  test         2  walking          fBodyAccMag -0.32428943 -0.5771052
+   4:  test         2  walking  fBodyBodyAccJerkMag -0.16906435 -0.1640920
+   5:  test         2  walking fBodyBodyGyroJerkMag -0.58324929 -0.5581046
+  ---                                                                     
+3056: train        30   laying        tBodyGyroJerk -0.06677954 -0.9816339
+3057: train        30   laying     tBodyGyroJerkMag -0.98508641 -0.9761771
+3058: train        30   laying         tBodyGyroMag -0.96228492 -0.9512644
+3059: train        30   laying          tGravityAcc  0.35646090 -0.9839231
+3060: train        30   laying       tGravityAccMag -0.96982998 -0.9601679
+```
+
+The resulting dataset is a data table with **3,060 rows** and **6 variables**.
+
+##Variable Description
+
+- `Group` identifies whether the subject belonged to either  *test* or *train* group.
+
+```R
+   Group    N Perc
+1:  test  918  0.3
+2: train 2142  0.7
+```
+
+- `SubjectID` identifies one of 30 subjects. It is therefore an *integer class variable* that ranges from 1 to 30.
+
+- `Activity` identifies one of 6 possible activites identified in the experimental design.
+
+```R
+             Activity   N      Perc
+1:            walking 510 0.1666667
+2:   walking_upstairs 510 0.1666667
+3: walking_downstairs 510 0.1666667
+4:            sitting 510 0.1666667
+5:           standing 510 0.1666667
+6:             laying 510 0.1666667
+```
+
+- `feature` refers to every feature and its subsequent transformation. See **Variable Transformation** section for more details. 
+
+```R
+ [1] fBodyAcc             fBodyAccJerk         fBodyAccMag         
+ [4] fBodyBodyAccJerkMag  fBodyBodyGyroJerkMag fBodyBodyGyroMag    
+ [7] fBodyGyro            tBodyAcc             tBodyAccJerk        
+[10] tBodyAccJerkMag      tBodyAccMag          tBodyGyro           
+[13] tBodyGyroJerk        tBodyGyroJerkMag     tBodyGyroMag        
+[16] tGravityAcc          tGravityAccMag 
+```
+
+- `mean` is the average of all mean scores for each `feature`. Some  *features* were measured across 3 axes (*XYZ*), in which case the corresponding average is across all 3 dimensions.
+
+- `std` is the analogous of `mean` for standard deviations across the same *features* as described above.
+
+##Variable Transformation
+
+All datasets were merged into one, and the relevant variables of mean and standard deviations of measurements extracted, along with subject , group, and activity id's.
+
+This data table was then molten and re-cast by feature measurement, aggregating across axes and activity to produce the tidy output dataset `tidydataoutput.txt`. 
+
+For more details around all transformation and data manipulation, please refer to the well commented [run_analysis.R](https://github.com/Jamamel/getcleanData/blob/master/run_analysis.R) script.
---
+++ run_analysisf.R
@@ -0,0 +1,100 @@
+# run_analysis.R wrapper function
+# wrapping the run_analysis.R into a function for benchmarking and
+# profiling purposes.
+
+output <- function(){
+	
+	######### Set WD to same folder as datasets.
+	
+	#### Load all relevant datasets into a list that mimics original folder structure
+	
+	## create list "datalist" containing all datasets
+	# create "datalist" structure based on folder/file structure in WD
+	pathlist <- alply(list.dirs(getwd(),recursive = F),1,list.files,pattern = '*.txt',full.names = T,recursive = T)
+	
+	# create dataset list, mimicing folder/file structure
+	datalist <- lapply(pathlist,sapply,
+										 function(x) {
+										 	initial <- read.table(x, nrows = 50)
+										 	classes <- sapply(initial, class)
+										 	tabAll <- data.table(read.table(x,colClasses = classes))
+										 })
+	
+	# name lists and elements within lists mimicing folder/file structure
+	groups <- list.dirs(recursive = F,full.names = F)
+	names(datalist) <- groups
+	
+	# names of individual data.tables become generic, removing "_test/trial.txt" substring
+	for(i in seq_along(groups)) names(datalist[[i]]) <- sub(paste('_',groups[i],'.txt',sep = ''),'',basename(names(datalist[[i]])))
+	
+	# cbind all data.tables in each list element of datalist
+	datalist2 <- lapply(datalist,function(x) do.call(cbind.data.frame,x))
+	
+	# add a test vs. trial identifier variable "Group" as a factor
+	# notice number of groups and datasets is dynamic based on original number of data folders
+	for(i in seq_along(groups)) datalist2[[i]][,Group := factor(groups[i],levels = groups)]
+	
+	# merge all datasets into single dataset using "rbind" action
+	fdata <- rbindlist(datalist2)
+	setnames(fdata,c('subject.V1','y.V1'),c('SubjectID','ActivityCode'))
+	setkeyv(fdata,c('Group','SubjectID','ActivityCode'))
+	
+	
+	## loads features metadata into R
+	metad <- fread('features.txt',colClasses = c('integer','character'))
+	setnames(metad,1:2,c('FeatureCode','FeatureDescr'))
+	setkey(metad,'FeatureCode')
+	
+	# identify feature codes for mean & std. deviation variables for each measure
+	# meanFreq feature is excluded due to ambiguity in requirement on course project description
+	extractcols <- grep('mean\\(\\)|std\\(\\)',metad$FeatureDescr,value = T)
+	
+	# extract feature codes
+	codes <- metad[metad$FeatureDescr %in% extractcols,'FeatureCode',with = F]
+	
+	# transform to match column names in fdata relating to code
+	xcodes <- paste('X.V',codes$FeatureCode,sep = '')
+	
+	# extracts relevant columns:
+	#		Group as test/trial group identifier
+	#		SubjectID as observation identifier
+	#		ActivityCode as activity identifier
+	#		xcodes variable names corresponding to all available mean & std. deviation measurments
+	measured <- fdata[,c(key(fdata),xcodes),with = F]
+	
+	# rename measurement varaibles to something cleaner and tidier
+	newnames <- metad[codes,'FeatureDescr',with = F]$FeatureDescr
+	newnames <- gsub('\\(\\)','',newnames)
+	setnames(measured,xcodes,newnames)
+	
+	# melt data.table
+	moltmeas <- melt(measured,id.vars = key(measured))
+	
+	# extract (transformed) signal, statistic calculated for said measurement, and dimensions
+	#		- axis takes vlues "X", "Y", or "Z", altneratively remaning missing (NA) for those signals averaged across all 3 dimensions (e.g. "fBodyBodyGyroJerkMag")
+	moltmeas[,variable := as.character(moltmeas$variable)]
+	
+	x <- strsplit(moltmeas$variable,split = '-')
+	x1 <- as.data.table(t(sapply(x,function(x) if(length(x) == 2) x = c(x,NA) else x)))
+	splitnames <- c('feature','stat','axis')
+	setnames(x1,splitnames)
+	moltmeas <- cbind(moltmeas,x1)
+	setkeyv(moltmeas,c('Group','SubjectID','ActivityCode'))
+	
+	# load activity codes into R
+	metad2 <- fread('activity_labels.txt',colClasses = c('integer','character'))
+	setnames(metad2,1:2,c('ActivityCode','ActivityDescr'))
+	setkey(metad2,'ActivityCode')
+	
+	# define activity code as factor and add labels
+	moltmeas[,ActivityCode := factor(moltmeas$ActivityCode,labels = tolower(metad2$ActivityDescr))]
+	setnames(moltmeas,'ActivityCode','Activity')
+	
+	# cast data.table to achieve tidy, workable format
+	# resulting data.table provides averages of mean & std. deviations by subject, activity, and feature (variable, across all 3 axes when available)
+	castdt <- dcast.data.table(moltmeas[,-c(4,8),with = F],Group + SubjectID + Activity + feature ~  stat,value.var = 'value',fun.aggregate = mean)
+	
+	# output as "tidydataoutput.txt" to working directory
+	write.table(castdt,file = 'tidydataoutput.txt',row.names = F)
+	
+}
